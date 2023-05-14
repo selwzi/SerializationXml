fa6f539ed6a798f1272ff09aacf3d96447cdab98
selwzi <ivshin.donel@gmail.com>
2023-05-15 00:12:26 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿using ConsoleApp1;
+
+RequestBenefit request = RequestBenefit.LoadFromXml("input.xml");
+request.OwnerFirstName = "Daniel";
+request.SaveToXml("output.xml");
---
+++ ConsoleApp1/RequestBenefit.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+    [Serializable]
+    public class RequestBenefit
+    {
+        public string OwnerLastName { get; set; }
+        public string OwnerFirstName { get; set; }
+        public string OwnerMiddleName { get; set; }
+        [XmlElement(DataType = "date")]
+        public DateTime OwnerBirthDate { get; set; }
+
+        public static RequestBenefit LoadFromXml(string file)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(RequestBenefit));
+            using (FileStream reader = new FileStream(file, FileMode.OpenOrCreate))
+            {
+                return serializer.Deserialize(reader) as RequestBenefit;
+            }
+        }
+
+        public void SaveToXml(string file)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(RequestBenefit));
+            using (FileStream writer = new FileStream(file, FileMode.OpenOrCreate))
+            {
+                serializer.Serialize(writer, this);
+            }
+        }
+    }
+}
